e2f36f382f1950964be1900f6c1c94a52a5da729
Emmett <emmettirl@gmail.com>
2025-02-10 23:52:01 +0000

    Beginning NoteReader Layout

--- composeApp/src/commonMain/kotlin/note/reader/App.kt
+++ composeApp/src/commonMain/kotlin/note/reader/App.kt
@@ -1,105 +1,136 @@
 package note.reader
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.datetime.Clock
-import kotlinx.datetime.IllegalTimeZoneException
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import notereader.composeapp.generated.resources.Res
-import notereader.composeapp.generated.resources.compose_multiplatform
-import notereader.composeapp.generated.resources.eg
-import notereader.composeapp.generated.resources.fr
-import notereader.composeapp.generated.resources.jp
-import notereader.composeapp.generated.resources.mx
-import notereader.composeapp.generated.resources.id
-import org.jetbrains.compose.resources.DrawableResource
 
 @Composable
 @Preview
-fun App(countries: List<Country> = defaultCountries) {
+fun App() {
     MaterialTheme {
-        var showCountries by remember { mutableStateOf(false) }
-        var timeAtLocation by remember { mutableStateOf("No location selected") }
+        MainLayout()
+    }
+}
 
-        Column(modifier = Modifier.padding(20.dp)) {
-            Text(
-                timeAtLocation,
-                style = TextStyle(fontSize = 20.sp),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
-            )
-            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
-                DropdownMenu(
-                    expanded = showCountries,
-                    onDismissRequest = { showCountries = false }
-                ) {
-                    countries.forEach { (name, zone, image) ->
-                        DropdownMenuItem(
-                            onClick = {
-                                timeAtLocation = currentTimeAt(name, zone)
-                                showCountries = false
-                            }
-                        ) {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Image(
-                                    painterResource(image),
-                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
-                                    contentDescription = "$name flag"
-                                )
-                                Text(name)
-                            }
-                        }
-                    }
-                }
-            }
-
-            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
-                onClick = { showCountries = !showCountries }) {
-                Text("Select Location")
-            }
+@Composable
+fun TwoPaneView() {
+    Row(modifier = Modifier.fillMaxSize()) {
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(8.dp)
+        ) {
+            DocumentReader()
+        }
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(8.dp)
+        ) {
+            NoteEditor()
         }
     }
 }
 
-fun currentTimeAt(location: String, zone: TimeZone): String {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
+@Composable
+fun DocumentReader(){
+    var documentReader by remember { mutableStateOf("Example Doc") }
 
-    val time = Clock.System.now()
-    val localTime = time.toLocalDateTime(zone).time
 
-    return "The time in $location is ${localTime.formatted()}"
+    Column {
+        Text(
+            text = "Document Reader",
+            style = TextStyle(fontSize = 20.sp),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.DarkGray),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = documentReader,
+                style = TextStyle(fontSize = 16.sp),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
-data class Country(val name: String, val zone: TimeZone, val image: DrawableResource)
+@Composable
+fun NoteEditor() {
+    var noteEditor by remember { mutableStateOf("") }
 
-val defaultCountries = listOf(
-    Country("Japan", TimeZone.of("Asia/Tokyo"), Res.drawable.jp),
-    Country("France", TimeZone.of("Europe/Paris"), Res.drawable.fr),
-    Country("Mexico", TimeZone.of("America/Mexico_City"), Res.drawable.mx),
-    Country("Indonesia", TimeZone.of("Asia/Jakarta"), Res.drawable.id),
-    Country("Egypt", TimeZone.of("Africa/Cairo"), Res.drawable.eg)
-)
+    Column {
+        Text(
+            text = "Notes Editor",
+            style = TextStyle(fontSize = 20.sp),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            TextField(
+                modifier = Modifier.fillMaxSize(),
+                value = noteEditor,
+                onValueChange = { noteEditor = it },
+                label = { Text("Notes") }
+            )
+        }
+    }
+}
+
+@Composable
+fun MainLayout() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        MenuBar("Top Menu")
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            TwoPaneView()
+        }
+        MenuBar("Bottom Menu")
+    }
+}
+
+@Composable
+fun MenuBar(title: String) {
+    Surface(
+        modifier = Modifier
+            .height(56.dp)
+            .background(Color.DarkGray)
+            .fillMaxWidth()
+    ) {
+        Text(text = title, color = Color.Black, textAlign = TextAlign.Center)
+    }
+}
